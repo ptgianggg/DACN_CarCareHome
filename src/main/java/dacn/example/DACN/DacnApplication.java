87e245f2f702d3d18d598c1ca7e2ef1d2573264e
ptgianggg <gp406509@gmail.com>
2026-03-09 19:18:22 +0700

    Khoi tao du an

---
+++ src/main/java/dacn/example/DACN/DacnApplication.java
@@ -0,0 +1,13 @@
+package dacn.example.DACN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dacn/example/DACN/DacnApplicationTests.java
@@ -0,0 +1,13 @@
+package dacn.example.DACN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
